e1b0892896e9af17f4c55b6f99f645925c19e690
liberbell <liberbell@gmail.com>
2019-03-02 23:31:54 +0900

    mean6

--- mean.go
+++ mean.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var x float32
-	var y float32
+	// var x float32
+	// var y float32
 
-	x = 1
-	y = 2
+	x := 1
+	y := 2
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-	var mean float32
-	mean = (x + y) / 2
+	// var mean float32
+	mean := (x + y) / 2
 	fmt.Printf("result: %v, type of %T\n", mean, mean)
 }
