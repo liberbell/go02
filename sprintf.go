b964e8da5a02fd8f4207ae6284b02d020cffa5c9
liberbell <liberbell@gmail.com>
2019-03-04 08:46:04 +0900

    sprintf4

--- sprintf.go
+++ sprintf.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	n := 42
 	s := fmt.Sprintf("%d", n)
-	fmt.Println("s = %v (Type %T)\n", n, n)
+	fmt.Printf("s = %v (Type %T)\n", n, n)
 	fmt.Printf("s = %v (Type %T)\n", s, s)
 }
