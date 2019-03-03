b5f01048cd2278e4905770a260c46dd4e9063706
liberbell <liberbell@gmail.com>
2019-03-03 15:17:13 +0900

    switch6

--- switch.go
+++ switch.go
@@ -1,18 +1,20 @@
 package main
 
-import fmt
+import (
+	"fmt"
+)
 
 func main() {
-  x := 2
+	x := 2
 
-  switch x {
-  case 1:
-    fmt.Println("one")
-  case 2:
-    fmt.Println("two")
-  case 3:
-    fmt.Println("three")
-  default:
-    fmt.Printf("many")
-  }
+	switch x {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	default:
+		fmt.Printf("many")
+	}
 }
