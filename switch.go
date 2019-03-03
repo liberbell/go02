ac919787610e712a870479779f3e6e076e95973c
liberbell <liberbell@gmail.com>
2019-03-03 15:07:22 +0900

    switch3

--- switch.go
+++ switch.go
@@ -1,16 +1,17 @@
 package main
 
-func main() {
+
+fucn main() {
   x := 2
 
-  swith x {
-    case 1:
-      fmt.Println("one")
-    case 2:
-      fmt.Println("two")
-    case 3:
-      fmt.Println("three")
-    default:
-      fmt.Printf("many")
-    }
+  switch x {
+  case 1:
+    fmt.Println("one")
+  case 2:
+    fmt.Println("two")
+  case 3:
+    fmt.Println("three")
+  default:
+    fmt.Printf("many")
+  }
 }
