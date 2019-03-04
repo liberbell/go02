67620b1195fe4a5e58d29ced9caa1010d52d274f
liberbell <liberbell@gmail.com>
2019-03-04 09:13:12 +0900

    slices12

--- slices.go
+++ slices.go
@@ -6,7 +6,7 @@ func main() {
 	loons := []string{"bugs", "daffy", "taz"}
 	fmt.Printf("loons = %v (Type %T)\n", loons, loons)
 
-	fmt.Println(len(loons)) // 3
+	fmt.Println(len(loons))
 	fmt.Println("--------")
 
 	fmt.Println(loons[0])
