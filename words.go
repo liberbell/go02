cfa16f20e53628e37a7cbe7f2182c60f894e0e1d
liberbell <liberbell@gmail.com>
2019-03-06 07:37:54 +0900

    words2

---
+++ words.go
@@ -0,0 +1,14 @@
+package main
+
+import "strings"
+
+func main() {
+	text := `
+  Needles and Pins
+  Needles and Pins
+  Sew me a sail
+  To catch me the wind
+  `
+
+	words := strings.Fields(text)
+}
