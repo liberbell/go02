67e90e08cc338f5c1950f9d49fe0f27f28498b7a
liberbell <liberbell@gmail.com>
2019-03-07 14:00:20 +0900

    errors3

--- errors.go
+++ errors.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func sqrt(n float64) (float64, error) {
+	if n < 0 {
+		return 0, 0, fmt.Errorf("sqrt of negative value (%f)", n)
+	}
+}
+
 func main() {
 	s1, err := sqrt(2.0)
 	if err != nil {
