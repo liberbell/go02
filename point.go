7e70003261b4d8183c19adbbb58b34e2f30be662
liberbell <liberbell@gmail.com>
2019-03-09 18:38:20 +0900

    point3

--- point.go
+++ point.go
@@ -1,11 +1,19 @@
 package main
 
+import "fmt"
+
 type Point struct {
 	x int
 	y int
 }
 
-func (p Point) move(dx int, dy int) {
+func (p Point) Move(dx int, dy int) {
 	p.x += dx
 	p.y += dy
+}
+
+func main() {
+	p := Point{1, 2}
+	p.Move(2, 3)
+	fmt.Printf("%+v\n", p)
 }
