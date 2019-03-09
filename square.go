9a655666cd448d76ec6e69e34e6c36a9235b952d
liberbell <liberbell@gmail.com>
2019-03-09 19:11:28 +0900

    square3

--- square.go
+++ square.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 type Square struct {
 	Center Point
 	Length int
+}
+
+func NewSquare(x int, y int, length int) (*Square, error) {
+	if length <= 0 {
+		return nil, fmt.Errorf("length must be >=0(was %s)", length)
+	}
 }
