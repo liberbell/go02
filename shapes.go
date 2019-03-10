99c754aa595acde4a0486560fe238cb722cf6376
liberbell <liberbell@gmail.com>
2019-03-10 14:39:46 +0900

    shapes8

--- shapes.go
+++ shapes.go
@@ -21,7 +21,7 @@ func (c *Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func samAreas(shapes []shapes) float64 {
+func sumAreas(shapes []Shape) float64 {
 	total := 0.0
 
 	for _, shape := range shapes {
