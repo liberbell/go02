2290688265852857c518f1bde5eddccb2de4a557
liberbell <liberbell@gmail.com>
2019-03-13 08:56:16 +0900

    sites2

--- sites.go
+++ sites.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func returnType(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	ctype := resp.Header.Get("content-type")
+	fmt.Printf("%s -> %s\n", url, ctype)
+}
