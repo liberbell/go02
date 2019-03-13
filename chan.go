4b3237abfbf2b4dfd011dc17deaaa645355b78d0
liberbell <liberbell@gmail.com>
2019-03-13 09:16:07 +0900

    chan3

--- chan.go
+++ chan.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	ch := make(chan int)
-	<-ch
-	fmt.Println("Here")
+	// ch := make(chan int)
+	// <-ch
+	// fmt.Println("Here")
+	go func() {
+		ch <- 353
+	}()
+	val := <-ch
+	fmt.Printf("got %d\n", val)
 }
