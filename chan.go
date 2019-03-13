0bde00e427126b6595b806f9af4813fa39936026
liberbell <liberbell@gmail.com>
2019-03-14 07:40:01 +0900

    chan1

--- chan.go
+++ chan.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	ch := make(chan int)
@@ -11,4 +14,17 @@ func main() {
 	}()
 	val := <-ch
 	fmt.Printf("got %d\n", val)
+
+	go func() {
+		for i := 0; i < 3; i++ {
+			fmt.Printf("sending %d\n", i)
+			ch <- i
+			time.Sleep(time.Second)
+		}
+	}()
+
+	for i := 0; i < 3; i++ {
+		val := <-ch
+		fmt.Printf("recieved %d\n", val)
+	}
 }
