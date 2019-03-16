60f9102158afc593c7cb1ede7c5a444283a74244
liberbell <liberbell@gmail.com>
2019-03-16 12:40:11 +0900

    cfg6

--- cfg.go
+++ cfg.go
@@ -19,4 +19,10 @@ func main() {
 	}
 
 	defer file.Close()
+
+	cfg := &Config{}
+	dec := toml.NewDecoder(file)
+	if err := dec.Decode(cfg); err != nil {
+		log.Fatalf("error: can`t decode configuration file - %s\n", err)
+	}
 }
