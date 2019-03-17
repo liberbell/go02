ae4c56d72292d56f2102e8ede53916d4be5c3267
liberbell <liberbell@gmail.com>
2019-03-17 14:08:02 +0900

    sqrt_test3

--- sqrt_test.go
+++ sqrt_test.go
@@ -1,5 +1,16 @@
 package sqrt
 
+import "testing"
+
 func almostEqual(v1, v2 float64) bool {
 	return Abs(v1-v2) <= 0.001
+}
+
+func TestSimple(t *testing.T) {
+	val, err := Sqrt(2)
+
+	if err != nil {
+		f.Fatalf("error in calculation - %s", err)
+	}
+
 }
