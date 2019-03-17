50607e73e3d3920ce64f850a1612200925f8d03f
liberbell <liberbell@gmail.com>
2019-03-18 07:42:38 +0900

    sqrt_test3

--- sqrt1/sqrt_test.go
+++ sqrt1/sqrt_test.go
@@ -45,39 +45,3 @@ func TestMany(t *testing.T) {
 		})
 	}
 }
-
-func TestSimple(t *testing.T) {
-	val, err := Sqrt(2)
-
-	if err != nil {
-		t.Fatalf("error in calculation - %s", err)
-	}
-
-	if !almostEqual(val, 1.414214) {
-		t.Fatalf("vad value - %f", val)
-	}
-}
-
-type testCase struct {
-	value    float64
-	expected float64
-}
-
-func TestMany(t *testing.T) {
-	testCases := []testCase{
-		{0.0, 0.0},
-		{2.0, 1.414214},
-		{9.0, 3.0},
-	}
-	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("%f", tc.value), func(t *testing.T) {
-			out, err := Sqrt(tc.value)
-			if err != nil {
-				t.Fatal("error")
-			}
-			if !almostEqual(out, tc.expected) {
-				t.Fatalf("%f != %f", out, tc.expected)
-			}
-		})
-	}
-}
