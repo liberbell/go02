930dad15e007abbb6102b4819dcbb9efb2865c32
liberbell <liberbell@gmail.com>
2019-03-17 14:37:48 +0900

    sqrt_test1

---
+++ sqrt1/sqrt_test.go
@@ -0,0 +1,51 @@
+package sqrt
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func almostEqual(v1, v2 float64) bool {
+	return Abs(v1-v2) <= 0.001
+}
+
+func TestMany(t *testing.T) {
+	file, err := os.Open("sqrt_cases.csv")
+}
+
+func TestSimple(t *testing.T) {
+	val, err := Sqrt(2)
+
+	if err != nil {
+		t.Fatalf("error in calculation - %s", err)
+	}
+
+	if !almostEqual(val, 1.414214) {
+		t.Fatalf("vad value - %f", val)
+	}
+}
+
+type testCase struct {
+	value    float64
+	expected float64
+}
+
+func TestMany(t *testing.T) {
+	testCases := []testCase{
+		{0.0, 0.0},
+		{2.0, 1.414214},
+		{9.0, 3.0},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%f", tc.value), func(t *testing.T) {
+			out, err := Sqrt(tc.value)
+			if err != nil {
+				t.Fatal("error")
+			}
+			if !almostEqual(out, tc.expected) {
+				t.Fatalf("%f != %f", out, tc.expected)
+			}
+		})
+	}
+}
