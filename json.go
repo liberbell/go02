a0f73d13b69b13a58ab6fe2fc26581b5bf80f1bf
liberbell <liberbell@gmail.com>
2019-03-19 08:30:06 +0900

    json3

--- json.go
+++ json.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
 var data = `
 {
   "user": "Scrooge McDuck",
@@ -7,19 +14,20 @@ var data = `
   "amount": 1000000.3
   }
   `
+
 type Request struct {
-  Login string `json:"user"`,
-  Type strign `json:"type"`,
-  Amount float64 `json:"amount"`
+	Login  string  `json:"user"`
+	Type   strign  `json:"type"`
+	Amount float64 `json:"amount"`
 }
 
 func main() {
-  rdr := bytes.NewBufferString(data)
+	rdr := bytes.NewBufferString(data)
 
-  dec := json.NewDecoder(rdr)
-  req := &Request{}
-  if err := dec.Decode(req); err != nil {
-    log.Fatalf("error: cant`t decode - %s", err)
-  }
-  fmt.Printf("got: %+v\n", req)
+	dec := json.NewDecoder(rdr)
+	req := &Request{}
+	if err := dec.Decode(req); err != nil {
+		log.Fatalf("error: cant`t decode - %s", err)
+	}
+	fmt.Printf("got: %+v\n", req)
 }
