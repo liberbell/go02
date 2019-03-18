e1c89ac15d136f7a062684b9e2873a108bc13930
liberbell <liberbell@gmail.com>
2019-03-19 08:39:55 +0900

    http2

--- http.go
+++ http.go
@@ -1,1 +1,11 @@
 package main
+
+func main() {
+  resp, err := http.Get("https://httpbin.org/get")
+  if err != nil {
+    log.Fatalf("error: can`t call httpbin.org")
+  }
+  defer resp.Body.Close
+
+  io.Copy(os.Stdout, resp.Body)
+}
