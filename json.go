bc748314e7990b5214e6a9d7dea9c0f77bb74f02
liberbell <liberbell@gmail.com>
2019-03-19 08:28:01 +0900

    json3

--- json.go
+++ json.go
@@ -12,3 +12,14 @@ type Request struct {
   Type strign `json:"type"`,
   Amount float64 `json:"amount"`
 }
+
+func main() {
+  rdr := bytes.NewBufferString(data)
+
+  dec := json.NewDecoder(rdr)
+  req := &Request{}
+  if err := dec.Decode(req); err != nil {
+    log.Fatalf("error: cant`t decode - %s", err)
+  }
+  fmt.Printf("got: %+v\n", req)
+}
