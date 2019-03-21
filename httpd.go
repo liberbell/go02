c68c3669982679b59019e6a7cf32011dffd06240
liberbell <liberbell@gmail.com>
2019-03-21 12:23:31 +0900

    httpd6

--- httpd.go
+++ httpd.go
@@ -64,6 +64,7 @@ func mathHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+	http.Handlefunc("/math", mathandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
