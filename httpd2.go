f96673b240a4e49d0aef43024235a0406a40fe8a
liberbell <liberbell@gmail.com>
2019-03-21 12:38:29 +0900

    httpd-4

--- httpd2.go
+++ httpd2.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+)
 
 var (
 	db     = map[string]interface{}{}
@@ -10,4 +14,9 @@ var (
 type Entry struct {
 	Key   string      `json:"key"`
 	Value interface{} `json:"value"`
+}
+
+func sendResponse(entry *Entry, w http.ResponseWriter) {
+	w.Header().Set("content-type", "application/json")
+	enc := json.NewEncoder(w)
 }
